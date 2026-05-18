642f840eeb6e7437dfed68a07d40ef158a98b057
misha-cw <bigun.mykhaylo@gmail.com>
2026-05-18 15:17:30 +0300

    Fix user API endpoint URLs with spec

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/user/", include("user.urls"), name="users"),
+    path("api/users/", include("user.urls"), name="users"),
 ]
