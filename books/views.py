b8559dae70868346e3099fd5c63ca614ea5478b6
misha-cw <bigun.mykhaylo@gmail.com>
2026-05-18 17:30:44 +0300

    Implement CRUD functionality for Book model with viewset and routing

--- books/views.py
+++ books/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookListSerializer, BookSerializer
+
+
+class BookViewset(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
