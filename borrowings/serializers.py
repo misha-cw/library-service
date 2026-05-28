16d92d3d0c91bcb36cab5f8d9ba035502fd29692
misha-cw <bigun.mykhaylo@gmail.com>
2026-05-28 18:27:17 +0300

    Add BorrowingCreateSerializer for borrowing creation with inventory validation

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db import transaction
 
 from borrowings.models import Borrowing
 from books.serializers import BookSerializer, BookListSerializer
@@ -23,3 +24,23 @@ class BorrowingListSerializer(BorrowingSerializer):
 
 class BorrowingDetailSerializer(BorrowingSerializer):
     book = BookSerializer(read_only=True)
+
+
+class BorrowingCreateSerializer(BorrowingSerializer):
+    class Meta(BorrowingSerializer.Meta):
+        fields = BorrowingSerializer.Meta.fields + ("user",)
+        read_only_fields = BorrowingSerializer.Meta.read_only_fields + ("user",)
+
+    def validate(self, attrs):
+        book = attrs.get("book")
+        if book and book.inventory <= 0:
+            raise serializers.ValidationError("This book is currently unavailable.")
+        return attrs
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            book = validated_data.get("book")
+            if book:
+                book.inventory -= 1
+                book.save()
+            return super().create(validated_data)
