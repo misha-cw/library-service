0d03540c1e7f5f9d0a68412df81a5f76cf06433b
misha-cw <bigun.mykhaylo@gmail.com>
2026-09-16 21:23:02 +0300

    Update token lifetime settings in SIMPLE_JWT configuration

--- config/settings.py
+++ config/settings.py
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=100),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
 }
 
